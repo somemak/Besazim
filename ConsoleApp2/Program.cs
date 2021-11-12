52fff7f8ea98fdea5051a3429039616a1c35321d
somemak <some.mak@gmail.com>
2021-11-12 14:31:37 +0330

    Update Program.cs
    xcvcvxcv

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,8 +7,7 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Mehdi");
-            Console.WriteLine("Mehdi");
+ 
          
         }
     }
